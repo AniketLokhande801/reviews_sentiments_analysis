970fc48018ff36a5a14e3046cf1fdf9fdda3090f
AniketLokhande801 <153910692+AniketLokhande801@users.noreply.github.com>
2024-08-06 12:11:03 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,29 +1,20 @@
 # preprocess.py
 
-import re
-import pandas as pd
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
-stop_words = set(stopwords.words('english'))
+import string
 
 def preprocess_text(text):
-    if pd.isnull(text):
-        return ""  # or handle it in a way that suits your use case
-
-    # Convert to lowercase
+    # Convert text to lowercase
     text = text.lower()
-
-    # Remove punctuation and special characters
-    text = re.sub(r'\W', ' ', text)
-
-    # Tokenize
-    tokens = word_tokenize(text)
-
+    # Remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Tokenize text
+    words = word_tokenize(text)
     # Remove stopwords
-    filtered_tokens = [word for word in tokens if word not in stop_words]
-
-    # Join tokens back to string
-    preprocessed_text = ' '.join(filtered_tokens)
-
-    return preprocessed_text
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    # Rejoin words into a single string
+    return ' '.join(words)
